d46a30b37f2470b83fcfb4b548bc1f89bb3b58b9
amdadulhaque200 <amdadulhaquekhan500@gmail.com>
2025-11-14 22:13:23 +0600

    Created all files

---
+++ src/main/java/com/example/_207059_cv/Launcher.java
@@ -0,0 +1,9 @@
+package com.example._207059_cv;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(CVApplication.class, args);
+    }
+}
